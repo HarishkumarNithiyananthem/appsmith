b5915b447e7afc2ceef592a62544491a2adccfcf
harish.n <harish.n@anorasolutions.com>
2024-09-12 09:06:10 +0000

    System generated commit, initial commit

---
+++ pages/Page2/jsobjects/Global_Variables/Global_Variables.js
@@ -0,0 +1,15 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	random_inputs: [],
+	myFun1 (field_values) {
+
+		this.random_inputs = field_values
+		
+		
+	},
+	async myFun2 (field_values) {
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+	}
+}
